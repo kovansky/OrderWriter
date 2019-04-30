11cb12773cf2be8ac83dc513b6fec7547db675be
Kovansky <root@f4dev.me>
2019-04-30 14:26:52 +0200

    Create base files and structure (configs, schemas, windows)

---
+++ application/windows/launcher/start.go
@@ -0,0 +1,32 @@
+package launcher
+
+import (
+	"github.com/kovansky/rpgUniverse/application"
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+)
+
+var (
+	window *widgets.QMainWindow
+)
+
+func RunStart() {
+	window = widgets.NewQMainWindow(nil, 0)
+	window.SetWindowTitle(application.Language.App.Title)
+	window.SetAutoFillBackground(true)
+	window.SetPalette(application.Palettes.WindowPalette)
+	window.SetGeometry2(0, 0, 300, 800)
+
+	area := widgets.NewQWidget(window, core.Qt__Widget)
+
+	titleLabel := widgets.NewQLabel2(application.Language.App.Title, area, core.Qt__Widget)
+	titleLabel.SetPalette(application.Palettes.MainPalette)
+
+	// TODO: center
+	layout := widgets.NewQVBoxLayout2(area)
+	layout.AddWidget(titleLabel, 0, core.Qt__AlignTop)
+
+	area.SetLayout(layout)
+	window.SetCentralWidget(area)
+	window.Show()
+}
--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package OrderWriter
+
+import (
+	"fmt"
+	"github.com/kovansky/OrderWriter/application/windows/launcher"
+	"github.com/therecipe/qt/widgets"
+	"os"
+)
+
+var (
+	app *widgets.QApplication
+)
+
+func main() {
+	fmt.Println("Starting an application")
+
+	app = widgets.NewQApplication(len(os.Args), os.Args)
+
+	launcher.RunStart()
+
+	widgets.QApplication_Exec()
+}
